9c2d2f307f6d0675ef9aea425fad5985c0310865
JSBundela <jogendrabundela52@gmail.com>
2025-05-13 20:36:17 +0530

    Update medibot.py

--- medibot.py
+++ medibot.py
@@ -37,8 +37,8 @@ def load_llm(repo_id: str, token: str):
         temperature=0.5,
         task="text-generation",
         huggingfacehub_api_token=token,
-        #max_new_tokens=512
-        model_kwargs={"max_length": 512}
+        max_new_tokens=512
+        #model_kwargs={"max_length": 512}
         #model_kwargs={"max_new_tokens": 512} 
     )
 
