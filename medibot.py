c726e57b5fcadf21c1bbe8671ab7b01ac30127e6
JSBundela <jogendrabundela52@gmail.com>
2025-12-04 00:20:29 +0530

    Update medibot.py

--- medibot.py
+++ medibot.py
@@ -67,7 +67,7 @@ def load_llm(repo_id: str, token: str):
 '''
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 
-def load_endpoint(repo_id: str, token: str):
+'''def load_endpoint(repo_id: str, token: str):
     st.write("load_endpoint called with repo_id:", repo_id)
     st.write("token present?", bool(token))
     return HuggingFaceEndpoint(
@@ -78,7 +78,7 @@ def load_endpoint(repo_id: str, token: str):
         temperature=0.2,
         max_new_tokens=512,
         return_full_text=False                  # <- explicit, not inside model_kwargs
-    )
+    )'''
 
 #def load_llm(repo_id: str, token: str):
     #endpoint = load_endpoint(repo_id, token)
